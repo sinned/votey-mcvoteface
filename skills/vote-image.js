44cd574d1148d56eff6628cf65a2935e473942ad
Glitch (calico-comfort) <none>
2019-08-28 17:54:12 +0000

    🚛🐀 Checkpoint ./skills/vote-image.js:215455/12284

--- skills/vote-image.js
+++ skills/vote-image.js
@@ -1,33 +1,52 @@
 module.exports = function(controller) {
 
     controller.hears(['vote'], 'direct_message,direct_mention', function(bot, message) {
-      bot.reply(message, 'Ok, vote on this:');
+      const content = {
+          blocks: [
+            {
+                "text": "Would you like to play a game?",
+                "attachments": [
+                    {
+                        "text": "Choose a game to play",
+                        "fallback": "You are unable to choose a game",
+                        "callback_id": "wopr_game",
+                        "color": "#3AA3E3",
+                        "attachment_type": "default",
+                        "actions": [
+                            {
+                                "name": "game",
+                                "text": "Chess",
+                                "type": "button",
+                                "value": "chess"
+                            },
+                            {
+                                "name": "game",
+                                "text": "Falken's Maze",
+                                "type": "button",
+                                "value": "maze"
+                            },
+                            {
+                                "name": "game",
+                                "text": "Thermonuclear War",
+                                "style": "danger",
+                                "type": "button",
+                                "value": "war",
+                                "confirm": {
+                                    "title": "Are you sure?",
+                                    "text": "Wouldn't you prefer a good game of chess?",
+                                    "ok_text": "Yes",
+                                    "dismiss_text": "No"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+          ]
+      };
+
+      bot.reply(message, content);
+      console.log('sending vote message');
     });
-
-
-    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-    /* Utility function to format uptime */
-    function formatUptime(uptime) {
-        var unit = 'second';
-        if (uptime > 60) {
-            uptime = uptime / 60;
-            unit = 'minute';
-        }
-        if (uptime > 60) {
-            uptime = uptime / 60;
-            unit = 'hour';
-        }
-        if (uptime != 1) {
-            unit = unit + 's';
-        }
-
-        uptime = parseInt(uptime) + ' ' + unit;
-        return uptime;
-    }
 
 };
