f2cb9a0a9be5debf5b807d708f65b1f78190d993
Glitch (calico-comfort) <none>
2019-08-28 19:04:31 +0000

    🏇💑 Checkpoint ./skills/vote_image.js:215455/5085

--- skills/vote_image.js
+++ skills/vote_image.js
@@ -1,29 +1,75 @@
 module.exports = function(controller) {
   
-  var dashbot = require('dashbot')(process.env.DASHBOT_API_KEY).slack;
+  var dashbot = require('dashbot')(process.env.DASHBOT_API_KEY, {debug:true}).slack;
 
-  controller.hears(['vote'], 'direct_message,direct_mention', function(bot, message) {
+  controller.hears(['vote'], 'direct_message,direct_mention', function(bot, message) {   
+    bot.reply(message, getVoteContent());
+  });
+  
+  // receive an interactive message, and reply with a message that will replace the original
+  controller.on('block_actions', function(bot, message) {
+    dashbot.logIncoming(bot.identity, bot.team_info, message);
+    bot.reply(message, 'Thanks for your vote!');
+    bot.reply(message, getVoteContent());
+  });
+  
+  controller.hears('interactive', 'direct_message', function(bot, message) {
+    bot.reply(message, {
+        attachments:[
+            {
+                title: 'Do you want to interact with my buttons?',
+                callback_id: '123',
+                attachment_type: 'default',
+                actions: [
+                    {
+                        "name":"yes",
+                        "text": "Yes",
+                        "value": "yes",
+                        "type": "button",
+                    },
+                    {
+                        "name":"no",
+                        "text": "No",
+                        "value": "no",
+                        "type": "button",
+                    }
+                ]
+            }
+        ]
+    });
+});
+  
+  function getImages() {
+    var images = [
+      'https://api.slack.com/img/blocks/bkb_template_images/goldengate.png',
+      'https://api.slack.com/img/blocks/bkb_template_images/beagle.png"'
+    ];
+    return images;
+  }
+  
+  function getVoteContent() {
+    const images = getImages();
     const content = {
         blocks: [
           {
           "type": "image",
           "title": {
             "type": "plain_text",
-            "text": "Example Image",
+            "text": "Image A",
             "emoji": true
           },
-          "image_url": "https://api.slack.com/img/blocks/bkb_template_images/goldengate.png",
-          "alt_text": "Example Image"
+          "image_url": images[0],
+          "alt_text": "Image A"
         },
         {
           "type": "image",
           "title": {
             "type": "plain_text",
-            "text": "image1",
+            "text": "Image B",
             "emoji": true
           },
-          "image_url": "https://api.slack.com/img/blocks/bkb_template_images/beagle.png",
-          "alt_text": "image1"
+          "image_url": images[1],
+          "alt_text": "Image B"
         },
         {
           "type": "section",
@@ -59,14 +105,8 @@ module.exports = function(controller) {
         }
       ]
     };
-    bot.reply(message, content);
-  });
-  
-  // receive an interactive message, and reply with a message that will replace the original
-  controller.on('block_actions', function(bot, message) {
-    dashbot.logIncoming(bot.identity, bot.team_info, message);
-    bot.replyInteractive(message, 'Thanks for your vote!');
-    console.log('interactive message incoming', bot.identity, bot.team_info);
-  });
+    return content;
+  }
+
 
 };
