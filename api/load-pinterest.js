87f1b64a7a99c86eb80c9151b4ed5637a57e7f75
Glitch (nasal-concrete) <none>
2019-08-30 23:10:23 +0000

    ☄️🦄 Checkpoint ./package.json:215455/1396 ./api/load-pinterest.js:215455/397

--- api/load-pinterest.js
+++ api/load-pinterest.js
@@ -1,8 +1,22 @@
 'use strict';
 
+const axios = require('axios');
+
 class LoadFromPinterest {
   async load(req) {
     const pinterestRssUrl = 'https://www.pinterest.com/choijoy/home-inspiration.rss';
+    axios.get(pinterestRssUrl)
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
     let values;
     console.log('LOADING PINTEREST FROM API');
     return {
