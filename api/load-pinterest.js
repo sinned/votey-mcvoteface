17d509d016b61d06b008025d518b09b9a79197e1
Glitch (nasal-concrete) <none>
2019-08-30 23:33:26 +0000

    📡🛀 Checkpoint ./api/load-pinterest.js:215455/1157

--- api/load-pinterest.js
+++ api/load-pinterest.js
@@ -1,26 +1,33 @@
 'use strict';
 
-const axios = require('axios');
 let Parser = require('rss-parser');
 let parser = new Parser();
+const mongoose = require('mongoose');
+const Images = mongoose.model('Images');
 
 class LoadFromPinterest {
   async load(req) {
     console.log('LOADING PINTEREST FROM API');
     const pinterestRssUrl = 'https://www.pinterest.com/choijoy/home-inspiration.rss';
     let feed = await parser.parseURL(pinterestRssUrl);
-    console.log(feed.title);
  
     feed.items.forEach(item => {
       let description = item.content;
       if (description) {
-        let regex = /<img[^>]+src="([^">]+)"/;
+        let regex = /(<img[^>]+src=")([^">]+)"/;
         var foundImgSrc = description.match(regex);     
-        console.log('foundImgSrc', foundImgSrc);            
+        console.log('foundImgSrc', foundImgSrc[2]);  
+        if (foundImgSrc[2]) {
+          var newImage = new Images(
+            {
+              experiment: feed.title,
+              image_url: foundImgSrc[2],
+              name: item.title
+            }
+          );
+          newImage.save();
+        }
       }
-
-      console.log('title', item.title);
-      console.log('---\n');
     });
 
     let values;
